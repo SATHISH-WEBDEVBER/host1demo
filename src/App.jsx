0a84bbb3fc31e9b41345f85b74b06ce3c5f17884
SATHISH-WEBDEVBER <sathishms1589@gmail.com>
2025-10-03 18:44:23 +0530

    deploy for test

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,10 @@ function App() {
         <li>Vite</li>
         <li>JavaScript</li>
       </ul>
+      <p>Edit <code>src/App.jsx</code> and save to test HMR updates.</p>
+      <p>
+        Click on the Vite and React logos to learn more.
+      </p>
     </div>
   )
 }
